34b43f051f3c6dc0337f2679b25fe05d4be4eb15
cousi2344 <cousi2344@gmail.com>
2017-11-20 14:16:52 -0500

    Edit suite.py to be less complex, remove unneeded code.

--- tests/suite.py
+++ tests/suite.py
@@ -1,27 +1,18 @@
 """
-Runs all basic tests included in the tests folder.
-
-This script creates and runs all tests considered in the "basic" tests groups,
-as opposed to "advanced" tests.
+Runs all tests in the tests folder.
 """
 
 import pytest
 import sys
 
 if __name__ == '__main__':
-    common_path = r"tests/common"
-    pop_basic_path = r"tests/population"
-    org_basic_path = r"tests/organism"
-    analysis_basic_path = r"tests/analysis"
+    test_path = r"tests"
 
     pytest_args = ["-v",
                    "--junitxml=./output/junit_xml/junit_xml.log",
                    "--html=./output/html_report/html_report.html",
                    "--self-contained-html",
-                   common_path,
-                   pop_basic_path,
-                   org_basic_path,
-                   analysis_basic_path]
+                   test_path]
 
     pytest_args.extend(sys.argv)
     pytest.main(pytest_args)
